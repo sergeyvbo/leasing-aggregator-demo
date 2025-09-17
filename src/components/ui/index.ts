84734c4675f18d851522cb274e0fa29b395e7c2f
Sergei Bogachev <sergeyvbo@gmail.com>
2025-09-16 21:39:16 -0700

    fix imports

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,7 @@
 export { default as Header } from './Header';
-export { default as Modal } from './Modal';
 export { default as Stepper } from './Stepper';
 export { default as SuccessNotification } from './SuccessNotification';
+export { default as ProposalModal } from './ProposalModal';
+export { default as LeasingFilters } from './LeasingFilters';
+export { default as LeasingResults } from './LeasingResults';
+export { default as ActiveFilters } from './ActiveFilters';
